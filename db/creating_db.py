5382828d02183b27e4e12b92ae847ba12fdcf2dc
Az1mbek-Xak1mov <azimbekhakimov2@gmail.com>
2025-05-28 17:17:26 +0500

    add onstartup

--- db/creating_db.py
+++ db/creating_db.py
@@ -1,5 +1,5 @@
 from db.engine import engine
 from db.models import Base
 
-def create_database():
+async def create_database():
     Base.metadata.create_all(bind=engine)
--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 from bot.dispatcher import TOKEN
 from bot.handler import *
+
+
+async def on_startup():
+    await create_database()
+
+
 async def main() -> None:
     bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+    dp.startup.register(on_startup)
     await dp.start_polling(bot)
 
+
 if __name__ == "__main__":
-    create_database()
     logging.basicConfig(level=logging.INFO, stream=sys.stdout)
     asyncio.run(main())
