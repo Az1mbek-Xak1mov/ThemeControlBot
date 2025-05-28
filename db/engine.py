d32163d1f8472b51283f5ec720587ea0ed4a23b3
Az1mbek-Xak1mov <azimbekhakimov2@gmail.com>
2025-05-28 17:12:17 +0500

    add dotenv and fix bugs for database

--- db/engine.py
+++ db/engine.py
@@ -1,7 +1,13 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from dotenv import load_dotenv
 
-DB_URL = "postgresql+psycopg2://postgres:1@localhost:5432/countmessages"
+
+load_dotenv('.env')
+
+DB_URL = f"postgresql+psycopg2://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
 
 engine = create_engine(DB_URL)
 SessionLocal = sessionmaker(bind=engine)
