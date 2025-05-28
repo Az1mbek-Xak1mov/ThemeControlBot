4a1adaf0436439160e6d9cd0966f799a4144b554
Az1mbek-Xak1mov <azimbekhakimov2@gmail.com>
2025-05-28 18:12:00 +0500

    commit

--- db/engine.py
+++ db/engine.py
@@ -1,7 +1,7 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-
-DB_URL = "postgresql+psycopg2://postgres:1@localhost:5432/countmessages"
+DB_URL = f"postgresql+psycopg2://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
 
 engine = create_engine(DB_URL)
 SessionLocal = sessionmaker(bind=engine)
